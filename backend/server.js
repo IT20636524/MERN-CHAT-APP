63647f0836129c45cf58bc5362c59d679bca8c8d
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-12-28 23:11:26 +0530

    Db Config

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,27 @@ const express = require("express");
 const res = require("express/lib/response");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require('colors');
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("API is Running");
+app.get("/", (req, res) => {
+  res.send("API is Running");
 });
 
-app.get('/api/chat', (req,res)=>{
-    res.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-app.get('/api/chat/:id',(req,res)=>{
-    // console.log(req.params.id);
-    const singleChat = chats.find((c)=> c._id === req.params.id);
-    res.send(singleChat);
-})
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000 ,console.log(`Server Started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
