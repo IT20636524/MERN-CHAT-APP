e8ed822498255bd6aeb4c1b12c46dabcf0dbd839
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-12-30 01:01:32 +0530

    Register-Backend

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,19 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json()); //to accept JSON Data
+
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('api/user',userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
