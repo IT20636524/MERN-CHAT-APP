8688dbae27c0087cb0b9654c4ee8602afa072e84
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2023-01-05 13:45:04 +0530

    Complete My Chats componenet

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const colors = require('colors');
-const userRoutes = require('./routes/userRoutes');
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
-const {notFound ,errorHandler} =require('./middleware/errorMiddleware')
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json()); //to accept JSON Data
 
@@ -17,12 +25,12 @@ app.use(express.json()); //to accept JSON Data
 //   res.send("API is Running");
 // });
 
-app.use('/api/user',userRoutes);
-app.use('/api/chat',chatRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-//error handling middleware 
-app.use(notFound)
-app.use(errorHandler)
+//error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
